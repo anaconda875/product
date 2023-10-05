69693ce572dadb9798227699398435133dafce52
Bao Ngo <bao.ngo@aeris.net>
2023-10-05 09:58:57 +0700

    try to solve N+1

--- src/main/java/com/example/product/domain/model/Category.java
+++ src/main/java/com/example/product/domain/model/Category.java
@@ -2,6 +2,7 @@ package com.example.product.domain.model;
 
 import lombok.Data;
 import lombok.ToString;
+import org.hibernate.annotations.BatchSize;
 
 import javax.persistence.*;
 import java.util.List;
@@ -9,6 +10,20 @@ import java.util.List;
 @Entity
 @Table(name = "tbl_category")
 @Data
+@NamedEntityGraph(
+  name = "test",
+  attributeNodes = {
+    @NamedAttributeNode(value="children",subgraph="children"),
+  },
+  subgraphs = {
+    @NamedSubgraph(
+      name = "children",
+      attributeNodes = {
+        @NamedAttributeNode("children")
+      }
+    )
+  })
+@BatchSize(size = 10)
 public class Category {
 
   @Id
@@ -23,9 +38,12 @@ public class Category {
   private Category parent;
 
   @OneToMany(mappedBy = "parent", cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+  @ToString.Exclude
+  @BatchSize(size = 10)
   private List<Category> children;
 
   @OneToMany(mappedBy = "category")
+  @ToString.Exclude
   private List<Product> products;
 
 }
--- src/main/java/com/example/product/repository/CategoryRepository.java
+++ src/main/java/com/example/product/repository/CategoryRepository.java
@@ -1,7 +1,17 @@
 package com.example.product.repository;
 
 import com.example.product.domain.model.Category;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.persistence.EntityManager;
+import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+//  @EntityGraph(value = "test", type = EntityGraph.EntityGraphType.FETCH)
+  @Query("SELECT DISTINCT c FROM Category c JOIN FETCH c.children")
+  Optional<Category> findById(Long id);
+
 }
